67da1a21ea843b813fd4a6fa483444eba17deb9a
Daniel <cdeh88@gmail.com>
2022-12-21 22:22:06 -0400

    create button handlers and add some styles

--- src/components/content/food/FoodList.js
+++ src/components/content/food/FoodList.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import foodStyles from "./FoodList.module.css";
 import FoodItem from "./FoodItem";
 
 const FoodList = () => {
+  const [finalPrice, setFinalPrice] = useState(0);
+
   const FOODS = [
-    { name: "pizza", desc: "a tasty pizza", price: 9.99 },
-    { name: "burger", desc: "a tasty burger", price: 6.99 },
-    { name: "sushi", desc: "a tasty sushi", price: 12.99 },
-    { name: "chicken wings", desc: "a tasty chicken wings", price: 12.99 },
-    { name: "menudito", desc: "a tasty menudito", price: 5.99 },
+    { id: 1, name: "pizza", desc: "a tasty pizza", price: 9.99 },
+    { id: 2, name: "burger", desc: "a tasty burger", price: 6.99 },
+    { id: 3, name: "sushi", desc: "a tasty sushi", price: 12.99 },
+    {
+      id: 4,
+      name: "chicken wings",
+      desc: "a tasty chicken wings",
+      price: 12.99,
+    },
+    { id: 5, name: "menudito", desc: "a tasty menudito", price: 5.99 },
   ];
 
   const listItem = FOODS.map((food) => {
     return (
       <FoodItem
+        key={food.id}
         itemName={food.name}
         itemDesc={food.desc}
         itemPrice={food.price}
@@ -21,13 +29,43 @@ const FoodList = () => {
     );
   });
 
+  const increasePricehandler = () => {
+    let increasedPrice = finalPrice + 1;
+    setFinalPrice(increasedPrice);
+  };
+  
+  const decreasePricehandler = () => {
+    let decreasedPrice = finalPrice - 1;
+
+    if (decreasedPrice <= 0) {
+      setFinalPrice(0);
+    } else {
+      setFinalPrice(decreasedPrice);
+    }
+  };
+
   return (
     <div className={foodStyles.contentList}>
       <div className={foodStyles.content}>
         <ul>
           <h2>Food list</h2>
-          {listItem}.
+          {listItem}
         </ul>
+      </div>
+      <span>{finalPrice}</span>
+      <div className={foodStyles.buttonContainer}>
+        <button
+          className={foodStyles.priceButton}
+          onClick={decreasePricehandler}
+        >
+          decrement price
+        </button>
+        <button
+          className={foodStyles.priceButton}
+          onClick={increasePricehandler}
+        >
+          increment price
+        </button>
       </div>
     </div>
   );
