dd509a68cecb7a4b3a4580e6c7b87c8e24dc7b75
GabOhr <hidalgomargabriel@gmail.com>
2022-12-29 22:22:45 -0400

    add social media buttons

--- src/components/ui/footer/Footer.js
+++ src/components/ui/footer/Footer.js
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>We're Here for You</h3>
-      <span>First Avenue 763 | 0800 100077 mostwanted_b@mostwanted.com | © All
-      rights reserved 2023</span>
+      <h3 className={styles.h3}>The Most Wanted Burgers Here</h3>
+      <span>
+        First Avenue 763 | 0800 100077 mostwanted_b@mostwanted.com | © All
+        rights reserved 2023
+      </span>
+      <div className={styles.socialIcons}>
+        <a href="https://www.facebook.com/mostwantedburgers">
+          <FontAwesomeIcon icon={faFacebook} color="black"/> 
+        </a>
+        <a href="https://www.instagram.com/mostwantedburgers">
+          <FontAwesomeIcon icon={faInstagram} color="black"/>
+        </a>
+      </div>
     </footer>
   );
 };
