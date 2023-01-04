bd0a52c9f1005b7778c2e208a892c0ac78034a36
GabOhr <hidalgomargabriel@gmail.com>
2023-01-03 20:47:08 -0400

    add header changes

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -2,17 +2,19 @@ import React from "react";
 import stylesHeader from "./Header.module.css";
 import LinkButton from "./LinkButton";
 import { Route } from "react-router-dom";
+import logoMw from "./assets/font-graffiti-MW-logo.png";
 
 const Header = () => {
-  let textLink1 = 'Home'
-  let textLink2 = 'Menu'
-  let textLink3 = 'Contact us'
+  let textLink1 = "Home";
+  let textLink2 = "Menu";
+  let textLink3 = "Contact us";
 
   return (
     <div className={stylesHeader.navHeader}>
-      <LinkButton route='/'>{textLink1}</LinkButton>
-      <LinkButton route='/menu'>{textLink2}</LinkButton>
-      <LinkButton route='/contactus'>{textLink3}</LinkButton>
+      <img className={stylesHeader.logoMostWanted} src={logoMw} alt="MostWanted"/>
+      <LinkButton route="/">{textLink1}</LinkButton>
+      <LinkButton route="/menu">{textLink2}</LinkButton>
+      <LinkButton route="/contactus">{textLink3}</LinkButton>
     </div>
   );
 };
