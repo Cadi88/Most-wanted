8ddf591f8e71bb0f84d6d3046cd2fa84ab4013b1
Daniel <cdeh88@gmail.com>
2023-01-04 22:13:29 -0400

    adjust footer

--- client/src/components/ui/footer/Footer.js
+++ client/src/components/ui/footer/Footer.js
@@ -6,17 +6,16 @@ import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3 className={styles.h3}>The Most Wanted Burgers Here</h3>
       <span>
         First Avenue 763 | 0800 100077 mostwanted_b@mostwanted.com | © All
         rights reserved 2023
       </span>
       <div className={styles.socialIcons}>
         <a href="https://www.facebook.com/mostwantedburgers">
-          <FontAwesomeIcon icon={faFacebook} color="black"/> 
+          <FontAwesomeIcon icon={faFacebook} color="orange"/> 
         </a>
         <a href="https://www.instagram.com/mostwantedburgers">
-          <FontAwesomeIcon icon={faInstagram} color="black"/>
+          <FontAwesomeIcon icon={faInstagram} color="orange"/>
         </a>
       </div>
     </footer>
