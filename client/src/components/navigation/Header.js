c44c17d1916e8c6124ce90cc874af4a02076a34c
Daniel <cdeh88@gmail.com>
2023-01-23 22:07:57 -0400

    add media queries for small devices

--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -1,33 +1,45 @@
 import React from "react";
 import { Image } from "@mantine/core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faBars } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Header.module.css";
 import LinkButton from "./LinkButton";
 import mwLogo from "../../assets/svg/most_logo_XS.svg";
 
 const Header = () => {
-  let textLink1 = "MW";
+  let textLink1 = mwLogo;
   let textLink2 = "Menu";
   let textLink3 = "Contact us";
 
   return (
     <div className={styles.navHeader}>
+
       <div className={styles.buttonsContainer}>
         <LinkButton route="/">
           <div className={styles.smLogo}>
-            <Image src={mwLogo} alt="Most wanted" />
+            <Image src={textLink1} alt="Most wanted" />
           </div>
         </LinkButton>
         <LinkButton route="/api/menu">{textLink2}</LinkButton>
         <LinkButton route="/contactus">{textLink3}</LinkButton>
       </div>
+
+      <div className={styles.burgerMenuContainer}>
+        <FontAwesomeIcon
+          icon={faBars}
+          size="lg"
+          color="white"
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+
       <div>
         <FontAwesomeIcon
           icon={faSearch}
+          size="lg"
           color="white"
-          style={{ cursor: "pointer", margin: '30px' }}
+          style={{ cursor: "pointer", margin: "30px" }}
         />
       </div>
     </div>
